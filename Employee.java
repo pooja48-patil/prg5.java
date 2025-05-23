8037280ab704ead01710f1264cb8566574980610
pooja48_patil <poojabadappanavar89@gmail.com>
2025-05-23 10:59:43 +0530

    save file

---
+++ Employee.java
@@ -0,0 +1,10 @@
+public class Employee {
+    private String name;
+    private int id;
+    private double salary;
+    private String department;
+    private String address;
+
+    public Employee(String name, int id, double salary, String department , String address) {
+        this.name = name;
+        this.id = id;
