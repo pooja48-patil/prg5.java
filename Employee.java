e1a9eb66700ef20895dc1d182cfc7177782a1acb
pooja48_patil <poojabadappanavar89@gmail.com>
2025-05-23 11:03:49 +0530

    save file

--- Employee.java
+++ Employee.java
@@ -7,4 +7,13 @@ public class Employee {
 
     public Employee(String name, int id, double salary, String department , String address) {
         this.name = name;
-        this.id = id;
+        this.id = id;public void dispay() {
+            System.out.println("Employee Name: " + name);
+            System.out.println("Employee ID: " + id);
+            System.out.println("Salary: " + salary);
+            System.out.println("Department: " + department);
+            System.out.println("Address:"+ address);
+        }
+        public static void main(String[] args) {
+            Employee emp = new Employee("John Doe", 12345, 50000.0, "Engineering","Hubbali");
+    
